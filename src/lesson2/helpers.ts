2a2b8427def2e9e4a953321873eb246b8df1998d
Andrey Tychinskiy <tychinskij@uchi.ru>
2020-04-14 13:58:04 +0300

    init helpers

---
+++ src/lesson2/helpers.ts
@@ -0,0 +1,7 @@
+import { SplittedString } from "./calculator";
+
+export const isNumber = (item: string): boolean => !isNaN(Number(item));
+
+export const checkLengthEqualsOne = (splittedLine): boolean => {
+  return splittedLine.length === 1;
+};
