504cd01c048d26a19fb5f9253b1b0e84eb119a29
Andrey Tychinskiy <tychinskij@uchi.ru>
2020-04-15 21:12:23 +0300

    change entry

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: './src/sum.ts',
+  entry: './src/index.tsx',
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx', '.json']
   },
