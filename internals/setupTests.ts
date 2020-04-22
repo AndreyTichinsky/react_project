c1e4da31ab9dbcc342e9a3634ad11c9fc70e1b3e
Andrey Tychinskiy <tychinskij@uchi.ru>
2020-04-22 20:50:50 +0300

    add serializer from jest-emotion

--- internals/setupTests.ts
+++ internals/setupTests.ts
@@ -1,4 +1,7 @@
 import * as enzyme from "enzyme";
 import ReactSixteenAdapterfrom from "enzyme-adapter-react-16";
+import serializer from "jest-emotion";
 
 (enzyme as any).configure({ adapter: new ReactSixteenAdapterfrom() });
+
+expect.addSnapshotSerializer(serializer);
