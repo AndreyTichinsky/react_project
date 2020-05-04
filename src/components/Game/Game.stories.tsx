e29f9f903d2ef2e409de9d14793bd13d7b746765
Andrey Tychinskiy <tychinskij@uchi.ru>
2020-05-04 20:13:55 +0300

    game: update story

--- src/components/Game/Game.stories.tsx
+++ src/components/Game/Game.stories.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { withKnobs, number, boolean } from "@storybook/addon-knobs";
-import { Field } from "./components";
+import { withKnobs, number, boolean, text } from "@storybook/addon-knobs";
+import { Field, Menu, Entrance } from "./components";
 import { Game } from "./Game";
 
 export default {
@@ -14,7 +14,10 @@ export const instanceGame = () => (
     ySize={number("ySize", 10)}
     cellSize={number("cellSize", 50)}
     fieldComponent={Field}
-    updateSpeed={number("updateSpeed", 500)}
+    menuComponent={Menu}
+    entranceComponent={Entrance}
+    updateSpeed={text("updateSpeed", "slow")}
     gameInProgress={boolean("gameInProgress", false)}
+    nameIsSubmited={boolean("nameIsSubmited", false)}
   />
 );
