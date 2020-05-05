06536bfef519b3cf61bc5ee69636a76ccc25e01f
Andrey Tychinskiy <tychinskij@uchi.ru>
2020-05-05 17:08:22 +0300

    refactor Entrance component

--- src/components/Game/components/Entrance/Entrance.tsx
+++ src/components/Game/components/Entrance/Entrance.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState, useCallback } from "react";
+import React, { FC } from "react";
 import type { HandlerControllerEvent } from "types/menu";
 import styled from "@emotion/styled";
 /** @jsx jsx */
