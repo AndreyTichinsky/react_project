8dbcee21233f4fc81503efaaaa5effd16b7404a9
Andrey Tychinskiy <tychinskij@uchi.ru>
2020-06-21 15:30:37 +0300

    refactor Entrance

--- src/components/Entrance/Entrance.tsx
+++ src/components/Entrance/Entrance.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { FC } from "react";
 import type { HandlerControllerEvent } from "types/menu";
 import { BaseEntrance, entranceLabelColor } from "./Entrance.styled";
 /** @jsx jsx */
@@ -10,32 +10,26 @@ export interface EntranceProps {
   handleUsername: (name: string) => void;
 }
 
-export const Entrance: React.FC<EntranceProps> = ({
-  submitHandler,
-  username,
-  handleUsername,
-}) => {
-  return (
-    <BaseEntrance>
-      <form onSubmit={submitHandler}>
-        <label
-          css={css`
-            ${entranceLabelColor}
-          `}
-        >
-          Please, enter your name:
-          <input
-            className="username_input"
-            name="username"
-            type="text"
-            value={username}
-            onChange={(ev) =>
-              handleUsername((ev.target as HTMLInputElement).value)
-            }
-          />
-        </label>
-        <button className="submit_username_button">submit</button>
-      </form>
-    </BaseEntrance>
-  );
-};
+export const Entrance: FC<EntranceProps> = (props) => (
+  <BaseEntrance>
+    <form onSubmit={props.submitHandler}>
+      <label
+        css={css`
+          ${entranceLabelColor}
+        `}
+      >
+        Please, enter your name:
+        <input
+          className="username_input"
+          name="username"
+          type="text"
+          value={props.username}
+          onChange={(ev) =>
+            props.handleUsername((ev.target as HTMLInputElement).value)
+          }
+        />
+      </label>
+      <button className="submit_username_button">submit</button>
+    </form>
+  </BaseEntrance>
+);
