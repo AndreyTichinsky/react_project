a7fa87d973a9569df902819c84ec0a941fe0e630
Andrey Tychinskiy <tychinskij@uchi.ru>
2020-07-08 12:59:28 +0300

    add new GameHelper tests

--- src/modules/Game/GameHelper.test.tsx
+++ src/modules/Game/GameHelper.test.tsx
@@ -76,4 +76,63 @@ describe("Game helpers testing", () => {
       10,
     ]);
   });
+  it("generationGenerator test", () => {
+    const cache = helper.cacheNeighbours(helper.makeMatrix(4, 4, 0), 4, 4);
+    expect(
+      helper.generationGenerator(
+        [1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0],
+        cache
+      )
+    ).toEqual([1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0]);
+  });
+  it("cacheNeighbours test", () => {
+    expect(helper.cacheNeighbours(helper.makeMatrix(4, 4, 0), 4, 4)).toEqual({
+      "0": [15, 12, 13, 3, 1, 7, 4, 5],
+      "1": [12, 13, 14, 0, 2, 4, 5, 6],
+      "2": [13, 14, 15, 1, 3, 5, 6, 7],
+      "3": [14, 15, 12, 2, 0, 6, 7, 4],
+      "4": [3, 0, 1, 7, 5, 11, 8, 9],
+      "5": [0, 1, 2, 4, 6, 8, 9, 10],
+      "6": [1, 2, 3, 5, 7, 9, 10, 11],
+      "7": [2, 3, 0, 6, 4, 10, 11, 8],
+      "8": [7, 4, 5, 11, 9, 15, 12, 13],
+      "9": [4, 5, 6, 8, 10, 12, 13, 14],
+      "10": [5, 6, 7, 9, 11, 13, 14, 15],
+      "11": [6, 7, 4, 10, 8, 14, 15, 12],
+      "12": [11, 8, 9, 15, 13, 3, 0, 1],
+      "13": [8, 9, 10, 12, 14, 0, 1, 2],
+      "14": [9, 10, 11, 13, 15, 1, 2, 3],
+      "15": [10, 11, 8, 14, 12, 2, 3, 0],
+    });
+  });
+});
+
+describe("asserts", () => {
+  it("isNumber test", () => {
+    expect(helper.isNumber(NaN)).toBeFalsy();
+    expect(helper.isNumber(Infinity)).toBeFalsy();
+    expect(helper.isNumber(-Infinity)).toBeFalsy();
+    expect(helper.isNumber(null)).toBeFalsy();
+    expect(helper.isNumber(undefined)).toBeFalsy();
+    expect(helper.isNumber("")).toBeFalsy();
+    expect(helper.isNumber({})).toBeFalsy();
+    expect(helper.isNumber([])).toBeFalsy();
+    expect(helper.isNumber(-1)).toBeTruthy();
+    expect(helper.isNumber(0)).toBeTruthy();
+    expect(helper.isNumber(42)).toBeTruthy();
+  });
+  it("assertSizeValue", () => {
+    expect(helper.assertSizeValue(-1)).toBeFalsy();
+    expect(helper.assertSizeValue(0)).toBeFalsy();
+    expect(helper.assertSizeValue(1)).toBeTruthy();
+    expect(helper.assertSizeValue(100)).toBeTruthy();
+    expect(helper.assertSizeValue(999)).toBeTruthy();
+  });
+  it("assertPercentValue", () => {
+    expect(helper.assertPercentValue(-1)).toBeFalsy();
+    expect(helper.assertPercentValue(101)).toBeFalsy();
+    expect(helper.assertPercentValue(0)).toBeTruthy();
+    expect(helper.assertPercentValue(1)).toBeTruthy();
+    expect(helper.assertPercentValue(100)).toBeTruthy();
+  });
 });
