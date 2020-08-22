e9220fc3bd4ce7d927b096d1f5b76548b554dd16
Andrey Tychinskiy <tychinskij@uchi.ru>
2020-08-22 11:27:28 +0300

    add localStorage mock

--- jest.config.js
+++ jest.config.js
@@ -134,7 +134,7 @@ module.exports = {
   // runner: "jest-runner",
 
   // The paths to modules that run some code to configure or set up the testing environment before each test
-  // setupFiles: [],
+  setupFiles: ["<rootDir>/internals/browserMocks.js"],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
   setupFilesAfterEnv: ["<rootDir>/internals/setupTests.ts"],
