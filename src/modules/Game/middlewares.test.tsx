0560c20a9fa81d399b99bb2c4d93beebee3facd2
Andrey Tychinskiy <tychinskij@uchi.ru>
2020-08-25 22:13:32 +0300

    necessary fixes

--- src/modules/Game/middlewares.test.tsx
+++ src/modules/Game/middlewares.test.tsx
@@ -20,11 +20,13 @@ describe("filledPercentValidation test", () => {
     invoke(actions.setInitialPercent(10));
     expect(next).toHaveBeenCalledWith(actions.setInitialPercent(10));
   });
+
   it("when invoke setFieldState action expect next called with setFieldState action", () => {
     const { next, invoke } = create(filledPercentValidation);
     invoke(actions.setFieldState([0, 0, 0, 0]));
     expect(next).toHaveBeenCalledWith(actions.setFieldState([0, 0, 0, 0]));
   });
+
   it("when invoke setInitialPercent action with invalid value then dispatch called with percentValidationFailed", () => {
     const { store, invoke } = create(filledPercentValidation);
     invoke(actions.setInitialPercent(-1));
@@ -32,6 +34,7 @@ describe("filledPercentValidation test", () => {
       type: "percentValidationFailed",
     });
   });
+
   it("when invoke setInitialPercent action with not a number value then dispatch called with percentValidationFailed", () => {
     const { store, invoke } = create(filledPercentValidation);
     invoke(actions.setInitialPercent(NaN));
@@ -47,16 +50,19 @@ describe("sizeValidation test", () => {
     invoke(actions.setXSize(10));
     expect(next).toHaveBeenCalledWith(actions.setXSize(10));
   });
+
   it("when invoke setYSize action expect next called with setYSize action", () => {
     const { next, invoke } = create(sizeValidation);
     invoke(actions.setYSize(10));
     expect(next).toHaveBeenCalledWith(actions.setYSize(10));
   });
+
   it("when invoke setFieldState action expect next called with setFieldState action", () => {
     const { next, invoke } = create(sizeValidation);
     invoke(actions.setFieldState([0, 0, 0, 0]));
     expect(next).toHaveBeenCalledWith(actions.setFieldState([0, 0, 0, 0]));
   });
+
   it("when invoke setXSize action with invalid value then dispatch called with sizeValidationFailed", () => {
     const { store, invoke } = create(sizeValidation);
     invoke(actions.setXSize(-1));
@@ -64,6 +70,7 @@ describe("sizeValidation test", () => {
       type: "sizeValidationFailed",
     });
   });
+
   it("when invoke setXSize action with not a number value then dispatch called with sizeValidationFailed", () => {
     const { store, invoke } = create(sizeValidation);
     invoke(actions.setXSize(NaN));
